99c4a798e8fefa9d342ce16feb42e975f9ea5363
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-02-24 13:13:22 -0500

    localstorage

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ const countdownElTitle = document.getElementById('countdown-title')
 const countdownBtn = document.getElementById('countdown-button')
 const timeElements = document.querySelectorAll('span') // Return an array of html elements 
 
+const completeEl = document.getElementById('complete')
+const completeElInfo = document.getElementById('complete-info')
+const completeBtn = document.getElementById('complete-button') 
+
 
 let counddownTitle = ''
 let countdownDate = ''
 let countdownValue = Date //Date function
 let countdownActive 
+
+let savedCountdown 
 
 const second = 1000 // 1s === 1000ms
 const minute = second * 60
@@ -34,33 +40,43 @@ dateEl.setAttribute('min', today) // Can only select today or future date
 // let dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 )).toISOString().split("T")[0]
 //------------------------------------------// //------------------------------------------// 
 
+
+
 // Populate Countdown / Complete UT
 function updateDOM(){
     countdownActive = setInterval(() => {
         const now = new Date().getTime() // getTime() return in milliseconds since midnight January 1, 1970
         const distance = countdownValue - now // countdownValue is the future date value, bigger than now
-        console.log('distance', distance)
+        // console.log('distance', distance)
     
-        
         const days = Math.floor(distance / day )  // example 4.5 days => 4days and 5 is the remainder
         const hours = Math.floor((distance % day) / hour) // use the remainder to get the whole hours
         const minutes = Math.floor((distance % hour) / minute) // use the remainder to get the whole minutes
         const seconds = Math.floor((distance % minute) / second) // use the remainder to get the whole second
-        console.log(days, hours, minutes, seconds)
-    
-        // Populate Countdown
-        countdownElTitle.textContent = `${countdownTitle}`
-        timeElements[0].textContent = `${days}`
-        timeElements[1].textContent = `${hours}`
-        timeElements[2].textContent = `${minutes}`
-        timeElements[3].textContent = `${seconds}`
-    
+        // console.log(days, hours, minutes, seconds)
     
         // Hide Input
         inputContainer.hidden = true
-    
-        //Show CountDown
-        countdownEl.hidden =false
+
+        // If the countdown has ended, show complete
+        if (distance < 0){
+            countdownEl.hidden = true
+            clearInterval(countdownActive)
+            completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`
+            // Show Complete
+            completeEl.hidden = false
+        } else {
+            // Show the countdown in progress
+            // Populate Countdown
+            countdownElTitle.textContent = `${countdownTitle}`
+            timeElements[0].textContent = `${days}`
+            timeElements[1].textContent = `${hours}`
+            timeElements[2].textContent = `${minutes}`
+            timeElements[3].textContent = `${seconds}`
+            completeEl.hidden = true
+            //Show CountDown
+            countdownEl.hidden =false
+        }
     }, second)
 }
 
@@ -72,13 +88,20 @@ function updateCountdown(e){
     countdownTitle = e.target[0].value  // this[0].value
     countdownDate = e.target[1].value // this[1].value
   
+    savedCountdown = {
+        title: countdownTitle,
+        date: countdownDate
+    }
+    // localStorgae Objects must be saved as a String. Else it gives back [object object]
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown)) 
+
     // Check for valid date 
     if(countdownDate === ''){
         alert('Please select a date for countdown')
     } else {
-        // Get number version of current Date, updateDOM
+        // Get number version of future Date, updateDOM
         countdownValue = new Date(countdownDate).getTime()  // getTime() return in milliseconds since midnight January 1, 1970
-        console.log('future', countdownValue)
+        // console.log('future', countdownValue)
         updateDOM()
     }
 
@@ -89,6 +112,7 @@ function updateCountdown(e){
 function reset() {
     // Hide Countdowns, show Input
     countdownEl.hidden = true
+    completeEl.hidden = true
     inputContainer.hidden = false
 
     // Stop the countdown
@@ -100,6 +124,25 @@ function reset() {
 }
 
 
+function restorePreviousCountdown(){
+    // Get countdown from localStorage if available / true
+    if(localStorage.getItem('countdown')){
+        inputContainer.hidden = true
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'))
+        countdownTitle = savedCountdown.title
+        countdownDate = savedCountdown.date
+        // Plug back the future date back into countdownValue
+        countdownValue = new Date(countdownDate).getTime()
+        updateDOM
+    }
+}
+
+
 // Event Listener
 countdownForm.addEventListener('submit', updateCountdown)
 countdownBtn.addEventListener('click', reset)
+completeBtn.addEventListener('click', reset)
+
+
+// On Load, check localStorage
+restorePreviousCountdown()
